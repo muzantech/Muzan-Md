34f4ac986ba8ce59e9dd175fb90631028d479b1b
muzantech <muzantech1@gmail.com>
2025-01-12 09:34:50 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'My Sticker Pack',
-  author: 'My Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'M͟͞u͟͞z͟͞a͟͞n͟͞ ͟͞m͟͞d͟͞||',
+  author: 'ᴹᵘᶻᵃⁿᵗᵉᶜʰ ',
+  botName: "M͟͞u͟͞z͟͞a͟͞n͟͞ ͟͞m͟͞d͟͞||",
+  botOwner: 'jarix', // Your name
+  ownerNumber: '393716907857', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
